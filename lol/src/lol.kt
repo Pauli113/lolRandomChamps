0028f90dc84a31e913f56f6326d89059b058aec2
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2022-05-13 16:14:09 +0200

    Add files via upload
    init project

---
+++ lol/src/lol.kt
@@ -0,0 +1,23 @@
+class lol {
+    val topLaners = listOf("Aatrox","Akali","Amumu","Camille","Cassiopeia","Cho'Gath","Darius","Dr. Mundo","Fiora","Gangplank","Garen","Gnar",
+    "Gwen","Hecarim","Heimerdinger"," Illaoi","Irelia","Jax","Jayce","Karma","Kayle","Kennen","Kled","Lillia","Lucian"," Malphite","Maokai","Mordekaiser","Nasus",
+        "Nocturne","Olaf","Ornn","Pantheon","Poppy","Quinn","Renekton","Rengar","Riven","Rumble","Ryze","Sett","Shen","Singed","Sion","Sylas","Tahm Kench","Teemo","Tryndamere",
+        "Urgot","Vayne","Waldimir","Volibear","Wukong","Yasuo","Yorick","Zac","Twitch"
+    )
+    val junglers = listOf("Amumu","Diana","Dr. Mundo","Ekko","Elise","Evelynn","Fiddlesticks","Gragas","Graves","Hecarim","Ivern","Jarvan IV","Jax",
+    "Karthus","Kayn","Kha'Zix","Kindred","Kled","Lee Sin","Lillia","Master Yi","Nidalee","Nocturne","Nunu & Willump","Olaf","Poppy","Rammus","Rek'Sai",
+    "Rengar","Sejuani","Sett","Shaco","Shyvana","Skarner","Sylas","Taliyah","Trundle","Udyr","Vi","Viego","Volibear","Warwick","Wukong","Xin Zhao","Zac"
+    )
+    val midLaners = listOf("Aatrox","Aatrox","Akali","Akshan","Anivia","Annie","Aurelion Sol","Azir","Camille","Cassiopeia","Cho'Gath","Corki","Diana","Ekko","Fizz",
+        "Galio","Garen","Heimerdinger","Irelia","Ivern","Jayce","Kassadin","Katarina","LeBlanc","Lillia","Lissandra","Lucian","Lux","Malphite","Malzahar","Neeko",
+        "Nocturne","Nunu & Willump","Orianna","Pantheon","Pyke","Qiyana","Renekton","Rumble","Ryze","Swain","Sylas","Syndra","Talon","Twisted Fate","Twisted Fate",
+        "Vel'Koz","Viktor","Waldimir","Xerath","Yasuo","Xerath","Zed","Ziggs","Zilean","Zoe"
+    )
+    val adc = listOf("Aphelios","Ashe","Caitlyn","Draven","Ezreal","Jhin","Jinx","Kai'Sa","Karlista","Kog'Maw","Lucian","Miss Fortune","Sivir","Tristana",
+    "Twitch","Varus","Vayne","Xayah","Yasuo","Zeri","Cassiopeia","Senna","Swain","Ziggs")
+
+    val support = listOf("Alistar","Bard","Blitzcrank","Brand","Braum","Janna","Karma","Leona","Lulu","Lux","Malphite","Morgana","Nami","Nautilus","Pantheon",
+    "Pyke","Rakan","Renata Glasc","Sett","Sona","Soraka","Tahm Kench","Taric","Tresh","Vel'Koz","Xerath","Yuumi","Zilean","Zyra","Senna","Swain","Galio","Maokai",
+    "Poppy","Shaco","Shen","Zac")
+
+}
